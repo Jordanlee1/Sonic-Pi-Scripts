84db56a533ab783e212e81b75c584ca8922fc025
Jordanlee1 <58793090+Jordanlee1@users.noreply.github.com>
2020-01-21 14:11:10 -0800

    Add files via upload

---
+++ stranger_things_sonic_pi.rb
@@ -0,0 +1,18 @@
+live_loop :stranger_things do
+  play:c4
+  sleep 0.2
+  play:e4
+  sleep 0.2
+  play:g4
+  sleep 0.2
+  play:b4
+  sleep 0.2
+  play:c5
+  sleep 0.2
+  play:b4
+  sleep 0.2
+  play:g4
+  sleep 0.2
+  play:e4
+  sleep 0.2
+end
