03989a0badbc068009e06ca5160162dc93f42cab
Jordanlee1 <58793090+Jordanlee1@users.noreply.github.com>
2020-01-31 15:35:11 -0800

    Add files via upload

---
+++ challenge_f_code.rb
@@ -0,0 +1,21 @@
+banger="C:/Users/jordan_lee/Downloads/challenge_f/this_is_a_banger.wav"
+x=1
+
+sample banger, rate: x
+sleep 3
+
+4.times do
+  x=x-0.1
+  sample banger, rate: x
+  sleep 3
+end
+
+4.times do
+  x=x+0.1
+  sample banger, rate: x
+  sleep 3
+end
+
+with_fx :reverb do
+  sample banger, rate: 1.5
+end
